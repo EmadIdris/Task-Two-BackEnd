b8889459328fc7d62f6e01122be2499fcac58b28
Emad Idris <emadidris@outlook.com>
2022-06-03 02:06:11 +0300

    finish

--- src/models/index.js
+++ src/models/index.js
@@ -3,8 +3,15 @@ const userModel = require("./user-model");
 require("dotenv").config();
 
 const DATABASE_URL = process.env.DATABASE_URL;
-
-const sequelize = new Sequelize(DATABASE_URL);
+const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  }
+} : {};
+const sequelize = new Sequelize(DATABASE_URL,DATABASE_CONFIG);
 
 const UsersTable = userModel(sequelize, DataTypes);
 
